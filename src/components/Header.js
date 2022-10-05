ce5746b8f2a39b9f3b50db4e3197716f20a9b660
Arbaz Choudhary <arbazkhan61@gmail.com>
2022-10-05 16:30:15 +0530

    First

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../components/style.css";
+function Header() {
+    return (
+        <header>
+            <nav>
+                Home | Contact Us | About Us | Services
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Slider.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../components/style.css';
+
+function Slider() {
+    return (
+        <div>
+            <div className='slider'>
+<h3>Arbaz Choudhary </h3>
+
+            </div>
+        </div>
+    );
+}
+
+export default Slider;
